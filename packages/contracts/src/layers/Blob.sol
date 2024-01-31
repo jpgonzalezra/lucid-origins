3d7118fdf9be8b57cfe8be6afd83e5d5b3416da9
Joaquin Gonzalez <jpgonzalezra@gmail.com>
2024-01-30 22:04:37 -0300

    refactor: string concat

--- packages/contracts/src/layers/Blob.sol
+++ packages/contracts/src/layers/Blob.sol
@@ -73,29 +73,27 @@ contract Blob {
         string memory svgPath;
         Point memory mid = Point({ x: (points[0].x + points[1].x) / 2, y: (points[0].y + points[1].y) / 2 });
 
-        svgPath = string(abi.encodePacked("M", mid.x.toString(), ",", mid.y.toString()));
+        svgPath = string.concat("M", mid.x.toString(), ",", mid.y.toString());
 
         for (uint256 i = 0; i < points.length; i++) {
             Point memory p1 = points[(i + 1) % points.length];
             Point memory p2 = points[(i + 2) % points.length];
             Point memory midPoint = Point({ x: (p1.x + p2.x) / 2, y: (p1.y + p2.y) / 2 });
 
-            svgPath = string(
-                abi.encodePacked(
-                    svgPath,
-                    "Q",
-                    p1.x.toString(),
-                    ",",
-                    p1.y.toString(),
-                    ",",
-                    midPoint.x.toString(),
-                    ",",
-                    midPoint.y.toString()
-                )
+            svgPath = string.concat(
+                svgPath,
+                "Q",
+                p1.x.toString(),
+                ",",
+                p1.y.toString(),
+                ",",
+                midPoint.x.toString(),
+                ",",
+                midPoint.y.toString()
             );
         }
 
-        return string(abi.encodePacked(svgPath, "Z"));
+        return string.concat(svgPath, "Z");
     }
 
     function build(
@@ -109,31 +107,29 @@ contract Blob {
         pure
         returns (string memory)
     {
-        return string(
-            abi.encodePacked(
-                colorDefs,
-                '<path id="',
-                id.toString(),
-                '" d="',
-                h1,
-                'Z" stroke-width="2" stroke="black" transform-origin="center" fill="',
-                fillColor,
-                '">',
-                '<animate attributeName="d" values="',
-                h1,
-                ";",
-                h2,
-                ";",
-                h1,
-                '" dur="30s" id="',
-                id.toString(),
-                '-anim" repeatCount="indefinite"',
-                ' keysplines=".42 0 1 1; 0 0 .59 1; .42 0 1 1; 0 0 .59 1;',
-                ' .42 0 1 1; 0 0 .59 1; .42 0 1 1; 0 0 .59 1;"/>',
-                '<animateTransform attributeName="transform" type="rotate" ',
-                'from="0" to="100" dur="1000" repeatCount="indefinite" />',
-                "</path>"
-            )
+        return string.concat(
+            colorDefs,
+            '<path id="',
+            id.toString(),
+            '" d="',
+            h1,
+            'Z" stroke-width="2" stroke="black" transform-origin="center" fill="',
+            fillColor,
+            '">',
+            '<animate attributeName="d" values="',
+            h1,
+            ";",
+            h2,
+            ";",
+            h1,
+            '" dur="30s" id="',
+            id.toString(),
+            '-anim" repeatCount="indefinite"',
+            ' keysplines=".42 0 1 1; 0 0 .59 1; .42 0 1 1; 0 0 .59 1;',
+            ' .42 0 1 1; 0 0 .59 1; .42 0 1 1; 0 0 .59 1;"/>',
+            '<animateTransform attributeName="transform" type="rotate" ',
+            'from="0" to="100" dur="1000" repeatCount="indefinite" />',
+            "</path>"
         );
     }
 }
